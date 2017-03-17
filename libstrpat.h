08b0c05748beb1f1c3df8571e0dc13e010bf245d
Marc Volker Dickmann <marc-q@users.noreply.github.com>
2017-03-17 15:54:23 +0100

    Rewrite some things to improve the parsing.

--- libstrpat.h
+++ libstrpat.h
@@ -8,19 +8,22 @@
 
 #define PAT_ESCAPE '\\'
 
+#define PAT_FLAG_ESCAPED 0
+
 enum _pat_class
 {
 	PAT_CLASS_NONE = 0,
-	PAT_CLASS_ALPHA = 200,
+	PAT_CLASS_ALPHA = 'a',
 	PAT_CLASS_DIGIT,
 	PAT_CLASS_ALNUM,
+	PAT_CLASS_PRINT,
 	PAT_CLASS_BREAK
 };
 
 bool string_match_start (const char*, const char*);
 bool string_match_end (const char*, const char*);
 
-unsigned char* string_pat_compile (unsigned char*, const char*);
-bool string_pat_match (const char*, const unsigned char*);
+char* string_pat_compile (char*, const char*);
+bool string_pat_match (const char*, const char*);
 
 #endif /* __LIBSTRPAT_H__ */
