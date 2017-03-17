29bfd93fb647f989b403f579e5a0ade4235e258d
Marc Volker Dickmann <marc-q@users.noreply.github.com>
2017-03-17 15:56:05 +0100

    Rewrite some things to improve the parsing.

--- test.c
+++ test.c
@@ -5,7 +5,7 @@
 static void
 test_pattern (const char *str, const char *desc)
 {
-	unsigned char *pat = NULL;
+	char *pat = NULL;
 	pat = string_pat_compile (pat, desc);
 	
 	if (pat == NULL)
@@ -33,8 +33,8 @@ main (int argc, char *argv[])
 	test_pattern ("Test eins", "[:alpha:] eins");
 	test_pattern ("Test eins", "[:alpha:] dins");
 	
-	test_pattern ("1. Punkt eins.", "[:digit:]. [:alnum:]");
-	test_pattern (" 2. Punkt zwei.", "[:digit:]. [:alnum:]");
+	test_pattern ("1. Punkt eins.", "[:digit:]. [:print:]");
+	test_pattern (" 2. Punkt zwei.", "[:digit:]. [:print:]");
 	
 	test_pattern ("user@localhost.local", "[:alnum:]@[:alnum:].[:alpha:]");
 	
