86e21a02266d86965e0aa0cf8d58ac05beabcd15
Marc Volker Dickmann <marc-q@users.noreply.github.com>
2017-03-18 15:14:08 +0100

    Add support for multiple character classes.

--- libstrpat.h
+++ libstrpat.h
@@ -13,17 +13,33 @@
 enum _pat_class
 {
 	PAT_CLASS_NONE = 0,
-	PAT_CLASS_ALPHA = 'a',
+	PAT_CLASS_ALNUM = 'a',
+	PAT_CLASS_ALPHA,
+	
+	PAT_CLASS_BLANK,
+	
+	PAT_CLASS_CNTRL,
+	
 	PAT_CLASS_DIGIT,
-	PAT_CLASS_ALNUM,
+	
+	PAT_CLASS_GRAPH,
+	
+	PAT_CLASS_LOWER,
+	
 	PAT_CLASS_PRINT,
-	PAT_CLASS_BREAK
+	PAT_CLASS_PUNCT,
+	
+	PAT_CLASS_SPACE,
+	
+	PAT_CLASS_UPPER,
+	
+	PAT_CLASS_XDIGIT
 };
 
 bool string_match_start (const char*, const char*);
 bool string_match_end (const char*, const char*);
 
-char* string_pat_compile (char*, const char*);
+char* string_pat_compile (const char*);
 bool string_pat_match (const char*, const char*);
 
 #endif /* __LIBSTRPAT_H__ */
